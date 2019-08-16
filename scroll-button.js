800b0bc9d6cd9011fe3daeaa3402fb8307fa39df
Aleksey Polyakov <jimdigris@mail.ru>
2019-08-16 16:20:32 +0300

    в работе

--- scroll-button.js
+++ scroll-button.js
@@ -4,5 +4,23 @@
 'use strict';
 
 (function () {
-
+	
+	const appearsButton = document.querySelector('.appearsButton');
+	
+	window.onscroll = onScrollMouse;
+	appearsButton.addEventListener('click', onAppearsButtonClick);
+	
+	function onScrollMouse () {
+		if (window.pageYOffset > 100) {
+			appearsButton.style.display = 'block';
+			setTimeout(function (){appearsButton.style.opacity = '1';}, 300);
+		} else {
+			appearsButton.style.opacity= '0';
+			setTimeout(function (){appearsButton.style.display = 'none';}, 200);
+		}
+	}
+	
+	function onAppearsButtonClick () {
+		window.scrollTo(0,0);
+	}
 })();
